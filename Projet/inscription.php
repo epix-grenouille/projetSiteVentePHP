72756dd2c5b26f1f0c9b973e6816f909223faa1f
Roskai <93260994+Roskai@users.noreply.github.com>
2021-11-19 16:48:25 +0100

    Add files via upload

--- Projet/inscription.php
+++ Projet/inscription.php
@@ -1,9 +1,30 @@
 <?php require_once("inc/init.inc.php"); ?>
 <?php require_once("inc/haut.inc.php"); ?>
- 
+<?php
+if($_POST)
+{
+/*$membre = executeRequete("SELECT * FROM Membre WHERE pseudo='$_POST[pseudo]'");
+        if($membre->num_rows > 0)
+        {
+            $contenu .= "<div class='erreur'>Pseudo indisponible. Veuillez en choisir un autre svp.</div>";
+        }
+        else
+        {*/
+            // $_POST['mdp'] = md5($_POST['mdp']);
+            foreach($_POST as $indice => $valeur)
+            {
+                $_POST[$indice] = htmlEntities(addSlashes($valeur));
+            }
+            executeRequete("INSERT INTO Membre (Pseudo, mot_de_passe) VALUES ('$_POST[pseudo]', '$_POST[mdp]'");
+            $contenu .= "<div class='validation'>Vous êtes inscrit à notre site web. <a href=\"connexion.php\"><u>Cliquez ici pour vous connecter</u></a></div>";
+        //}
+    }
+?>
+
+<?php echo $contenu; ?>
 <form method="post" action="">
     <label for="pseudo">Pseudo</label><br>
-    <input type="text" id="pseudo" name="pseudo" maxlength="20" placeholder="votre pseudo" pattern="[a-zA-Z0-9-_.]{1,20}" title="caractères acceptés : a-zA-Z0-9-_." required="required"><br><br>
+    <input type="text" id="pseudo" name="pseudo" maxlength="20" placeholder="Votre pseudo" required="required"><br><br>
           
     <label for="mdp">Mot de passe</label><br>
     <input type="password" id="mdp" name="mdp" required="required"><br><br>
